955d56f78733f4b46d351b6617c6ae08cbec4c75
Bartlomiej <szybiakbartlomiej@gmail.com>
2023-03-23 12:11:35 +0100

    Testing method about Password control

---
+++ src/main/java/org/example/testgorilla/Password.java
@@ -0,0 +1,11 @@
+package org.example.testgorilla;
+
+public class Password {
+    public static void main(String[] args) {
+        Password.controlStringPasswords(new String[]{"P@sswORD1","20passWORD20","PASS6word"},"4dro6");
+    }
+    public static  String controlStringPasswords(String password[],String control){
+        System.out.println(control);
+        return "";
+    }
+}
